b3b52c82f66a5612b857e8ad5a928f2e1fe26a57
Leko <leko.noor@gmail.com>
2020-01-05 19:50:47 +0900

    Update snapshot

--- __tests__/e2e.test.js
+++ __tests__/e2e.test.js
@@ -2,27 +2,27 @@ const { spawnSync } = require('child_process')
 
 describe('E2E', () => {
     it('--help', () => {
-        const { status, output: [, stdout, stderr] } = spawnSync('node', ['../index.js', '--help'])
+        const { status, output: [, stdout, stderr] } = spawnSync('node', ['../index.js', '--help'], { encoding: 'utf8' })
         expect(status).toBe(0)
         expect({ stdout, stderr }).toMatchSnapshot()
     })
     it('--set', () => {
-        const { status, output: [, stdout, stderr] } = spawnSync('node', ['../index.js', '--set'])
+        const { status, output: [, stdout, stderr] } = spawnSync('node', ['../index.js', '--set'], { encoding: 'utf8' })
         expect(status).toBe(0)
         expect({ stdout, stderr }).toMatchSnapshot()
     })
     it('--set -f', () => {
-        const { status, output: [, stdout, stderr] } = spawnSync('node', ['../index.js', '--set', '-f'])
+        const { status, output: [, stdout, stderr] } = spawnSync('node', ['../index.js', '--set', '-f'], { encoding: 'utf8' })
         expect(status).toBe(0)
         expect({ stdout, stderr }).toMatchSnapshot()
     })
     it('--set -f --https', () => {
-        const { status, output: [, stdout, stderr] } = spawnSync('node', ['../index.js', '--set', '-f', '--https'])
+        const { status, output: [, stdout, stderr] } = spawnSync('node', ['../index.js', '--set', '-f', '--https'], { encoding: 'utf8' })
         expect(status).toBe(0)
         expect({ stdout, stderr }).toMatchSnapshot()
     })
     it('--set -f --no-shallow', () => {
-        const { status, output: [, stdout, stderr] } = spawnSync('node', ['../index.js', '--set', '-f', '--no-shallow'])
+        const { status, output: [, stdout, stderr] } = spawnSync('node', ['../index.js', '--set', '-f', '--no-shallow'], { encoding: 'utf8' })
         expect(status).toBe(0)
         expect({ stdout, stderr }).toMatchSnapshot()
     })
