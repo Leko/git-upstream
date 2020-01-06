77712e325b140e84d4301d648e64449ae099be59
Leko <leko.noor@gmail.com>
2020-01-06 14:44:23 +0900

    Remove snapshots

--- __tests__/e2e.test.js
+++ __tests__/e2e.test.js
@@ -4,26 +4,21 @@ describe('E2E', () => {
     it('--help', () => {
         const { status, output: [, stdout, stderr] } = spawnSync('node', ['../index.js', '--help'], { encoding: 'utf8' })
         expect(status).toBe(0)
-        expect({ stdout, stderr }).toMatchSnapshot()
     })
     it('--set', () => {
         const { status, output: [, stdout, stderr] } = spawnSync('node', ['../index.js', '--set'], { encoding: 'utf8' })
         expect(status).toBe(0)
-        expect({ stdout, stderr }).toMatchSnapshot()
     })
     it('--set -f', () => {
         const { status, output: [, stdout, stderr] } = spawnSync('node', ['../index.js', '--set', '-f'], { encoding: 'utf8' })
         expect(status).toBe(0)
-        expect({ stdout, stderr }).toMatchSnapshot()
     })
     it('--set -f --https', () => {
         const { status, output: [, stdout, stderr] } = spawnSync('node', ['../index.js', '--set', '-f', '--https'], { encoding: 'utf8' })
         expect(status).toBe(0)
-        expect({ stdout, stderr }).toMatchSnapshot()
     })
     it('--set -f --no-shallow', () => {
         const { status, output: [, stdout, stderr] } = spawnSync('node', ['../index.js', '--set', '-f', '--no-shallow'], { encoding: 'utf8' })
         expect(status).toBe(0)
-        expect({ stdout, stderr }).toMatchSnapshot()
     })
 })
