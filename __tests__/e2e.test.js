d7b64358406db7b622732346b75714b881ad15c1
Leko <leko.noor@gmail.com>
2020-01-06 15:32:10 +0900

    Add assertion

--- __tests__/e2e.test.js
+++ __tests__/e2e.test.js
@@ -2,23 +2,28 @@ const { spawnSync } = require('child_process')
 
 describe('E2E', () => {
     it('--help', () => {
-        const { status, output: [, stdout, stderr] } = spawnSync('node', ['../index.js', '--help'], { encoding: 'utf8' })
+        const { status, output: [, , stderr] } = spawnSync('node', ['../index.js', '--help'], { encoding: 'utf8' })
+        expect(stderr).toBe('')
         expect(status).toBe(0)
     })
     it('--set', () => {
-        const { status, output: [, stdout, stderr] } = spawnSync('node', ['../index.js', '--set'], { encoding: 'utf8' })
+        const { status, output: [, , stderr] } = spawnSync('node', ['../index.js', '--set'], { encoding: 'utf8' })
+        expect(stderr).toBe('')
         expect(status).toBe(0)
     })
     it('--set -f', () => {
-        const { status, output: [, stdout, stderr] } = spawnSync('node', ['../index.js', '--set', '-f'], { encoding: 'utf8' })
+        const { status, output: [, , stderr] } = spawnSync('node', ['../index.js', '--set', '-f'], { encoding: 'utf8' })
+        expect(stderr).toBe('')
         expect(status).toBe(0)
     })
     it('--set -f --https', () => {
-        const { status, output: [, stdout, stderr] } = spawnSync('node', ['../index.js', '--set', '-f', '--https'], { encoding: 'utf8' })
+        const { status, output: [, , stderr] } = spawnSync('node', ['../index.js', '--set', '-f', '--https'], { encoding: 'utf8' })
+        expect(stderr).toBe('')
         expect(status).toBe(0)
     })
     it('--set -f --no-shallow', () => {
-        const { status, output: [, stdout, stderr] } = spawnSync('node', ['../index.js', '--set', '-f', '--no-shallow'], { encoding: 'utf8' })
+        const { status, output: [, , stderr] } = spawnSync('node', ['../index.js', '--set', '-f', '--no-shallow'], { encoding: 'utf8' })
+        expect(stderr).toBe('')
         expect(status).toBe(0)
     })
 })
