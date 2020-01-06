47f65a4825bdeea729c70b3cc38024810b7339de
Leko <leko.noor@gmail.com>
2020-01-06 15:57:54 +0900

    fix lint errors

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 #!/usr/bin/env node
 const chalk = require("chalk");
-const tap = require("lodash/tap");
 const flow = require("lodash/flow");
 const yargs = require("yargs");
 const git = require("simple-git/promise")();
